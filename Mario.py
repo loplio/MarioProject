6069da2971ee7c89991c4cef3524fdf2efe45bf7
loplio <jgh210@naver.com>
2021-10-09 13:23:35 +0900

    #3 기본이동구현

---
+++ Mario.py
@@ -0,0 +1,44 @@
+from pico2d import *
+
+class Mario:
+    def __init__(self):
+        self.x, self.y = 400, 70
+        self.frame = 0
+        self.image = load_image('Basic.png')
+        self.dir = 0
+        self.jump = False
+        self.acceleration = 0
+        self.buffer_y = 0
+    def update(self):
+        # self.frame = (self.frame + 1) % 8
+        self.x += 1.5*self.dir
+        self.y += self.acceleration
+        self._jump()
+        self.handle_events()
+    def draw(self):
+        self.image.draw_now(self.x, self.y)
+        # self.image.clip_draw(self.frame*40, 0, 40, 40, self.x, self.y)
+    def _jump(self):
+        if self.jump == True:
+            print(self.acceleration)
+            self.acceleration = self.acceleration - 0.98 * 0.1
+            if(self.buffer_y > self.y):
+                print(self.buffer_y, self.y)
+                self.acceleration = 0
+                self.y = self.buffer_y
+                self.jump = False
+    def handle_events(self):
+        events = get_events()
+        for event in events:
+            if event.type == SDL_KEYDOWN:
+                if event.key == SDLK_RIGHT:
+                    self.dir = 1
+                elif event.key == SDLK_LEFT:
+                    self.dir = -1
+                elif event.key == SDLK_UP:
+                    self.jump = True
+                    self.acceleration = 5
+                    self.buffer_y = self.y
+            elif event.type == SDL_KEYUP:
+                if event.key == SDLK_RIGHT or event.key == SDLK_LEFT:
+                    self.dir = 0
--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-if __name__ == '__main__':
+from pico2d import *
+from Mario import *
+Game_loop = True
 
-    pass
+def handle_events():
+    global Game_loop
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            Game_loop = False
+        elif event.type == SDL_QUIT and event.key == SDLK_ESCAPE:
+            Game_loop = False
+
+open_canvas()
+mario = Mario()
+while Game_loop:
+    start_time = get_time()
+    clear_canvas()
+    mario.draw()
+    mario.update()
+close_canvas()
